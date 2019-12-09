a1cbd13c3dfafd3657a86764c20cf95a070a123d
thilaknagaraj <thilak.gowda@gmail.com>
2019-12-09 10:14:49 +0530

    removed dupicate header component moidied the code to UI

--- src/Components/Custome-Button/Custome-Button.component.jsx
+++ src/Components/Custome-Button/Custome-Button.component.jsx
@@ -4,7 +4,7 @@ import './Custome-Button.style.scss';
 
 
 const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button class= {`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button `}
+    <button class= {`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
         {...otherProps}>
         {children}
 
